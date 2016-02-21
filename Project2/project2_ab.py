2fd6b7cec2d7b3fcf4fd4f513bf5ac65222a92e1
amethlex <amethlex@gmail.com>
2016-02-21 15:19:59 -0800

    add the comments of two files

--- Project2/project2_ab.py
+++ Project2/project2_ab.py
@@ -13,7 +13,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from matplotlib.ticker import FormatStrFormatter
 import nltk.stem
 
-
+# remove stem when calculating TF.IDF
 english_stemmer = nltk.stem.SnowballStemmer('english')
 class StemmedTfidfVectorizer(TfidfVectorizer):
 
@@ -21,12 +21,12 @@ class StemmedTfidfVectorizer(TfidfVectorizer):
         analyzer = super(StemmedTfidfVectorizer, self).build_analyzer()
         return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
         
-
+# choose 8 required classes
 cat=['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
                 'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
 train = f20(subset='train',categories=cat, shuffle = True, random_state = 42)
 
-
+#printing number of documents per target(class)
 x=np.arange(0,9,1)
 fig, ax = plt.subplots()
 counts, bins, patches = ax.hist(train.target,x, facecolor='red', edgecolor='gray')
@@ -46,9 +46,9 @@ for count, x in zip(counts, bin_centers):
         xytext=(0, -32), textcoords='offset points', va='top', ha='center')
 plt.title('Number of Documents per Target')
 plt.subplots_adjust(bottom=0.15)
-
+# choose all 20 classes this time
 train = f20(subset='train',shuffle = True, random_state = 42)
-
+# get the number of features
 stopwords = text.ENGLISH_STOP_WORDS
 vectorizer = StemmedTfidfVectorizer(
     min_df=1, stop_words=stopwords, decode_error='ignore')
@@ -57,17 +57,20 @@ tfidf=vector.toarray()
 print 'number of terms: '+str(len(tfidf[0]))
 cat={0:'alt.atheism',1:'alt.atheism',2:'comp.graphics',3:'comp.os.ms-windows.misc',4:'comp.sys.ibm.pc.hardware',5:'comp.windows.x',6:'misc.forsale',7:'rec.autos',8:'rec.motorcycles',9:'rec.sport.baseball',10:'rec.sport.hockey',11:'sci.crypt',12:'sci.electronics',13:'sci.med',14:'sci.space',15:'soc.religion.christian',16:'talk.politics.guns',17:'talk.politics.mideast',18:'talk.politics.misc',19:'talk.religion.misc'
 }
+# make the number of each classes become the same
 number_of_document=[]
 raw_data=[]
+#   get the datasets of different classes and the number of documents
 for i in range(0,20):
     tmp_data=f20(subset='train',categories=[cat[i]], shuffle = True, random_state = 42)
     number_of_document.append(len(tmp_data.data))
     raw_data.append(tmp_data.data)
+#   get the minimum number
 intend_number=min(number_of_document)
+#   reduce the number of each classes to the same intended number
 for i in range(0,20):
-    print len(raw_data[i])
     raw_data[i]=raw_data[i][0:intend_number]
-    print len(raw_data[i])
+#   achieve new datasets and corresponding targets
 new_data=[]
 new_target=[]
 for i in range(0,20):
--- Project2/project2_c.py
+++ Project2/project2_c.py
@@ -11,28 +11,18 @@ Created on Thu Feb 18 17:11:50 2016
 
 @author: fengjun
 """
-from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 from sklearn.datasets import fetch_20newsgroups as f20
 from sklearn.feature_extraction import text
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 import nltk.stem
-import scipy
-
-english_stemmer = nltk.stem.SnowballStemmer('english')
-class StemmedTfidfVectorizer(TfidfVectorizer):
-
-    def build_analyzer(self):
-        analyzer = super(StemmedTfidfVectorizer, self).build_analyzer()
-        return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
-        
+import scipy       
 class StemmedCountVectorizer(CountVectorizer):
 
     def build_analyzer(self):
         analyzer = super(StemmedCountVectorizer, self).build_analyzer()
         return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
-        
+# combine the documents of the same classes into the same docuemnt, divided by ' '        
 train = f20(subset='train',shuffle = True, random_state = 42)
 datalist=[]
 for i in range(0,20):
@@ -40,20 +30,22 @@ for i in range(0,20):
 for i in range(0,len(train.data)):
     datalist[train.target[i]]+=(' '+train.data[i])
     
-
+# get the count vector
 stopwords = text.ENGLISH_STOP_WORDS
 vectorizer = StemmedCountVectorizer(
     min_df=1, stop_words=stopwords, decode_error='ignore')
 vector = vectorizer.fit_transform(datalist)
 count=vector.toarray()
-
+# get the if and icf
 index={0:3,1:4,2:6,3:15}
 tf=np.ndarray([4, len(count[0])], dtype=float)
 icf=np.ndarray([4, len(count[0])], dtype=float)
+# calculating if
 for i in index:
     s=float(np.sum(count[index[i]]))
     for j in range(0,len(count[0])):
         tf[i][j]=0.5+0.5*count[index[i]][j]/s
+# calculating icf
 for i in index:
     for j in range(0,len(count[0])):
         c=0
@@ -61,6 +53,8 @@ for i in index:
             if(count[x][j]!=0):
                 c=1+c
         icf[i][j]=scipy.log(float(20)/c)
+# multiply if and icf, thus achiveing if.icf
+# get the most 10 significent terms in the 4 classes
 tficf=tf*icf
 print '\n'+'comp.sys.ibm.pc.hardware'
 indices = np.argsort(tficf[0])[::-1]

8b600da436e12f489192790e5b851fecdacfd17f
amethlex <amethlex@gmail.com>
2016-03-04 22:29:13 -0800

    update all

--- Project3/submission/problem1.m
+++ Project3/submission/problem1.m
@@ -1,11 +1,17 @@
 clear;
 load data;
-[A1,Y1]=wnmfrule(data,10);
-error1=squareError(data,A1,Y1);
-[A2,Y2]=wnmfrule(data,50);
-error2=squareError(data,A2,Y2);
-[A3,Y3]=wnmfrule(data,100);
-error3=squareError(data,A3,Y3);
-disp(['when k is 10,  the total least squared error is ' num2str(error1)])
-disp(['when k is 50,  the total least squared error is ' num2str(error2)])
-disp(['when k is 100,  the total least squared error is ' num2str(error3)])
+iteration=[50,100,200,500,1000,2000];
+for i=1:1:6
+    option.iter=iteration(i);
+    option.dis=false;
+    [A1,Y1]=wnmfrule(data,10,option);
+    error1=squareError(data,A1,Y1);
+    [A2,Y2]=wnmfrule(data,50,option);
+    error2=squareError(data,A2,Y2);
+    [A3,Y3]=wnmfrule(data,100,option);
+    error3=squareError(data,A3,Y3);
+    disp(['Iteration is ' num2str(iteration(i))])
+    disp(['when k is 10,  the total least squared error is ' num2str(error1)])
+    disp(['when k is 50,  the total least squared error is ' num2str(error2)])
+    disp(['when k is 100,  the total least squared error is ' num2str(error3)])
+end
--- Project3/submission/problem3.m
+++ Project3/submission/problem3.m
@@ -1,44 +1,102 @@
 clear;
 load data;
 obervation=[userId,itemId,rating];
-error=zeros(1,10);
-indices = crossvalind('Kfold',100000,10);
-result=zeros(2,10);
-option.iter=1000;
-option.dis=false;
+iteration=[50,100,200];
+AUC=zeros(1,3);
+figure(1)
+hold on
+title('Precision-recall Curve for k=100 under Different Iterations')
+xlabel('Recall') 
+ylabel('Precision')
+for j=1:1:3
+    error=zeros(1,10);
+    indices = crossvalind('Kfold',100000,10);
+    result=zeros(2,10);
+    option.iter=iteration(j);
+    option.dis=false;
 
-test = (indices == 1); 
-train = ~test;
-R=NaN*ones(943,1682);
-trainset=obervation(train,:);
-for m=1:1:90000
-     curuser=trainset(m,1);
-     curitem=trainset(m,2);
-     currating=trainset(m,3);
-     R(curuser,curitem)=currating;
+    test = (indices == 1); 
+    train = ~test;
+    R=NaN*ones(943,1682);
+    trainset=obervation(train,:);
+    for m=1:1:90000
+         curuser=trainset(m,1);
+         curitem=trainset(m,2);
+         currating=trainset(m,3);
+         R(curuser,curitem)=currating;
+    end
+
+    [A,Y,numIter,tElapsed,finalResidual]=wnmfrule(R,100,option);
+    P=A*Y;
+    testset=obervation(test,:);
+    prerating=NaN*ones(1,10000);
+
+    for i=1:1:10000
+        tmpuserId=testset(i,1);
+        tmpitemId=testset(i,2);
+        prerating(i)=P(tmpuserId,tmpitemId);
+    end
+
+    precisions=zeros(1,1001);
+    recalls=zeros(1,1001);
+    for i=0:1:1000
+        [precision,recall]=precisionAndRecall(testset(:,3),prerating,i*0.01);
+        precisions(i+1)=precision;
+        recalls(i+1)=recall;
+    end;
+
+
+    plot(recalls,precisions)
+    AUC(j)=-trapz(recalls,precisions);
 end
+legend(['iteration= ' num2str(iteration(1)) ',AUC= ' num2str(AUC(1))],['iteration= ' num2str(iteration(2)) ',AUC= ' num2str(AUC(2))],['iteration= ' num2str(iteration(3)) ',AUC= ' num2str(AUC(3))])
 
-[A,Y,numIter,tElapsed,finalResidual]=wnmfrule(R,100,option);
-P=A*Y;
-testset=obervation(test,:);
-prerating=NaN*ones(1,10000);
+figure(2)
+hold on
+iteration=[10,50,100];
+AUC=zeros(1,3);
+title('Precision-recall Curve for iteration=100 under Different k')
+xlabel('Recall') 
+ylabel('Precision')
+for j=1:1:3
+    error=zeros(1,10);
+    indices = crossvalind('Kfold',100000,10);
+    result=zeros(2,10);
+    option.iter=100;
+    option.dis=false;
 
-for i=1:1:10000
-    tmpuserId=testset(i,1);
-    tmpitemId=testset(i,2);
-    prerating(i)=P(tmpuserId,tmpitemId);
+    test = (indices == 1); 
+    train = ~test;
+    R=NaN*ones(943,1682);
+    trainset=obervation(train,:);
+    for m=1:1:90000
+         curuser=trainset(m,1);
+         curitem=trainset(m,2);
+         currating=trainset(m,3);
+         R(curuser,curitem)=currating;
+    end
+
+    [A,Y,numIter,tElapsed,finalResidual]=wnmfrule(R,iteration(j),option);
+    P=A*Y;
+    testset=obervation(test,:);
+    prerating=NaN*ones(1,10000);
+
+    for i=1:1:10000
+        tmpuserId=testset(i,1);
+        tmpitemId=testset(i,2);
+        prerating(i)=P(tmpuserId,tmpitemId);
+    end
+
+    precisions=zeros(1,1001);
+    recalls=zeros(1,1001);
+    for i=0:1:1000
+        [precision,recall]=precisionAndRecall(testset(:,3),prerating,i*0.01);
+        precisions(i+1)=precision;
+        recalls(i+1)=recall;
+    end;
+
+
+    plot(recalls,precisions)
+    AUC(j)=-trapz(recalls,precisions);
 end
-
-precisions=zeros(1,1001);
-recalls=zeros(1,1001);
-for i=0:1:1000
-    [precision,recall]=precisionAndRecall(testset(:,3),prerating,i*0.01);
-    precisions(i+1)=precision;
-    recalls(i+1)=recall;
-end;
-
-figure
-plot(recalls,precisions)
-xlabel('Recall'); ylabel('Precision')
-AUC=-trapz(recalls,precisions);
-title(['Precision-recall curve (AUC=' num2str(AUC) ')'])
+legend(['k= ' num2str(iteration(1)) ',AUC= ' num2str(AUC(1))],['k= ' num2str(iteration(2)) ',AUC= ' num2str(AUC(2))],['k= ' num2str(iteration(3)) ',AUC= ' num2str(AUC(3))])

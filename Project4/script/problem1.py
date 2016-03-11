086a34389885b71de5c630f8154f71426b29b89b
yuliqiang <1198693608@qq.com>
2016-03-10 23:21:17 -0800

    update problem1

--- Project4/script/problem1.py
+++ Project4/script/problem1.py
@@ -25,38 +25,16 @@ def loadTweet(txtname):
         
         
 
-#hashtag = ['gohawks', 'gopatriots', 'nfl', 'patriots', 'sb49', 'superbowl']
-#for i in range(1):
-#    tweets = loadTweet(hashtag[i])
-#    start_date = datetime.datetime(2015,02,01, 12,0,0)
-#    end_date = datetime.datetime(2015,02,01, 12,0,0)
-#    mintime = int(time.mktime(start_date.timetuple()))
-#    maxtime = int(time.mktime(end_date.timetuple()))
-#    usrid = set()
-#    num_followers = 0
-#    num_retweet = 0
-#    for j in range(0, len(tweets)):
-#        tweet = tweets[j]
-#        tweet_time = tweet['firstpost_date']
-#        mintime = min(mintime, tweet_time)
-#        maxtime = max(maxtime, tweet_time)
-#        usrid.add(tweet['tweet']['user']['id'])
-#        num_followers += tweet['tweet']['user']['followers_count']
-#        num_retweet += tweet['tweet']['retweet_count']
-#    hourlength = math.ceil((maxtime-mintime)/3600)
-#    print 'The average number of tweets per hour for #%s is %f\n' %(hashtag[i], len(tweets)/hourlength)
-#    print 'The average number of followers of users for #%s is %f\n' %(hashtag[i], num_followers/len(usrid))
-#    print 'The average number of retweet for #%s is %f\n' %(hashtag[i], num_retweet/len(tweets))
-
-#hashtag_plot = ['nfl', 'superbowl']
-hashtag_plot = ['gohawks']
-for i in range(len(hashtag_plot)):
-    tweets = loadTweet(hashtag_plot[i])
+hashtag = ['gohawks', 'gopatriots', 'nfl', 'patriots', 'sb49', 'superbowl']
+for i in range(1):
+    tweets = loadTweet(hashtag[i])
     start_date = datetime.datetime(2015,02,01, 12,0,0)
     end_date = datetime.datetime(2015,02,01, 12,0,0)
     mintime = int(time.mktime(start_date.timetuple()))
-    maxtime = int(time.mktime(end_date.timetuple())) 
-    
+    maxtime = int(time.mktime(end_date.timetuple()))
+    usrid = set()
+    num_followers = 0
+    num_retweet = 0
     for j in range(0, len(tweets)):
         tweet = tweets[j]
         tweet_time = tweet['firstpost_date']
@@ -64,17 +42,41 @@ for i in range(len(hashtag_plot)):
         if(mintime-new_mintime < 3600*24*15):
             mintime = new_mintime
         maxtime = max(maxtime, tweet_time)
-     
-    freq = []
-    for k in range(0, len(tweets)):
-        tweet = tweets[k]
-        if(tweet['firstpost_date'] < mintime):
-            continue
-        freq.append(math.ceil((tweet['firstpost_date']-mintime)/3600))
-    
-    plt.figure()    
-    plt.hist(freq, 494)
-    plt.xlabel('hours')
-    plt.ylabel('frequency')
-    plt.title('The number of tweets VS hour for #%s' %hashtag_plot[i])
+        usrid.add(tweet['tweet']['user']['id'])
+        num_followers += tweet['tweet']['user']['followers_count']
+        num_retweet += tweet['tweet']['retweet_count']
+    hourlength = math.ceil((maxtime-mintime)/3600)
+    print 'The average number of tweets per hour for #%s is %f\n' %(hashtag[i], len(tweets)/hourlength)
+    print 'The average number of followers of users for #%s is %f\n' %(hashtag[i], num_followers/len(usrid))
+    print 'The average number of retweet for #%s is %f\n' %(hashtag[i], (num_retweet+0.0)/len(tweets))
+
+#hashtag_plot = ['nfl', 'superbowl']
+#hashtag_plot = ['gohawks']
+#for i in range(len(hashtag_plot)):
+#    tweets = loadTweet(hashtag_plot[i])
+#    start_date = datetime.datetime(2015,02,01, 12,0,0)
+#    end_date = datetime.datetime(2015,02,01, 12,0,0)
+#    mintime = int(time.mktime(start_date.timetuple()))
+#    maxtime = int(time.mktime(end_date.timetuple())) 
+#    
+#    for j in range(0, len(tweets)):
+#        tweet = tweets[j]
+#        tweet_time = tweet['firstpost_date']
+#        new_mintime = min(mintime, tweet_time)
+#        if(mintime-new_mintime < 3600*24*15):
+#            mintime = new_mintime
+#        maxtime = max(maxtime, tweet_time)
+#     
+#    freq = []
+#    for k in range(0, len(tweets)):
+#        tweet = tweets[k]
+#        if(tweet['firstpost_date'] < mintime):
+#            continue
+#        freq.append(math.ceil((tweet['firstpost_date']-mintime)/3600))
+#    
+#    plt.figure()    
+#    plt.hist(freq, 494)
+#    plt.xlabel('hours')
+#    plt.ylabel('frequency')
+#    plt.title('The number of tweets VS hour for #%s' %hashtag_plot[i])
 

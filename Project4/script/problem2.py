fe681209f1cbc0e22057fe66cc792462eeb118f1
Jun Feng <amethlex@gmail.com>
2016-03-18 23:08:58 -0700

    Update problem2.py

--- Project4/script/problem2.py
+++ Project4/script/problem2.py
@@ -8,16 +8,21 @@ Created on Wed Mar  9 13:07:37 2016
 import numpy as np
 from sklearn import linear_model
 import math
+import statsmodels.api as sm
 
 
 hashtag = ['gohawks', 'gopatriots', 'nfl', 'patriots', 'sb49', 'superbowl']
 for i in range(len(hashtag)):
-    filename = 'problem3_data_#%s' %hashtag[i]
+    filename = 'problem2_data_#%s' %hashtag[i]
     data_tmp = np.loadtxt(filename)
     if(i == 0):
         data = data_tmp
     else:
         data = np.concatenate((data,data_tmp))
     
-source = data[0:len(data)-1,1:]
-target = data[1:,0]
+    source = data[0:len(data)-1,:]
+    target = data[1:,0]
+    mod = sm.OLS(target,source)
+    res = mod.fit()
+    print("****The Hashtag is {0:s}*******************".format(hashtag[i]))
+    print res.summary()

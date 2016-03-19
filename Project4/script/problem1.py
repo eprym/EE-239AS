e0ab496fc79036693a8cffb4acb4d8608416bd78
yuliqiang <1198693608@qq.com>
2016-03-18 22:38:15 -0700

    update problem6

--- Project4/script/problem1.py
+++ Project4/script/problem1.py
@@ -10,80 +10,46 @@ import math
 import datetime, time
 from matplotlib import pyplot as plt
 
-#def loadTweet(txtname):
-#    filename = '../tweet_data/tweets_#%s.txt' % txtname
-#    f = open(filename)
-#    line = f.readline()
-#    tweet = json.loads(line)
-#    
-#    tweets = []
-#    while(len(line) != 0):
-#        tweet = json.loads(line)
-#        tweets.append(tweet)
-#        line = f.readline()
-#    return tweets
+
         
         
 
 hashtag = ['gohawks', 'gopatriots', 'nfl', 'patriots', 'sb49', 'superbowl']
-#for i in range(5,6):
-#    tweets = loadTweet(hashtag[i])
-#    start_date = datetime.datetime(2015,02,01, 12,0,0)
-#    end_date = datetime.datetime(2015,02,01, 12,0,0)
-#    mintime = int(time.mktime(start_date.timetuple()))
-#    maxtime = int(time.mktime(end_date.timetuple()))
-#    usrid = set()
-#    num_followers = 0
-#    num_retweet = 0
-#    for j in range(0, len(tweets)):
-#        tweet = tweets[j]
-#        tweet_time = tweet['firstpost_date']
-#        new_mintime = min(mintime, tweet_time)
-#        if(mintime-new_mintime < 3600*24*15):
-#            mintime = new_mintime
-#        maxtime = max(maxtime, tweet_time)
-#        usrid.add(tweet['tweet']['user']['id'])
-#        num_followers += tweet['author']['followers']
-#        num_retweet += tweet['metrics']['citations']['total']
-#    hourlength = math.ceil((maxtime-mintime)/3600)
-#    print 'The average number of tweets per hour for #%s is %f\n' %(hashtag[i], len(tweets)/hourlength)
-#    print 'The average number of followers of users for #%s is %f\n' %(hashtag[i], num_followers/len(usrid))
-#    print 'The average number of retweet for #%s is %f\n' %(hashtag[i], (num_retweet+0.0)/len(tweets))
 
 
-#for i in range(0,len(hashtag)):
-#    filename = '../tweet_data/tweets_#%s.txt' % hashtag[i]
-#    f = open(filename)
-#    line = f.readline()
-#    
-#    start_date = datetime.datetime(2015,02,01, 12,0,0)
-#    end_date = datetime.datetime(2015,02,01, 12,0,0)
-#    mintime = int(time.mktime(start_date.timetuple()))
-#    maxtime = int(time.mktime(end_date.timetuple()))
-#    usrid = set()
-#    num_followers = 0
-#    num_retweet = 0
-#    num_tweet = 0;
-#    
-#    while(len(line) != 0):
-#        tweet = json.loads(line)
-#        num_tweet += 1
-#        tweet_time = tweet['firstpost_date']
-#        new_mintime = min(mintime, tweet_time)
-#        if(mintime-new_mintime < 3600*24*15):
-#            mintime = new_mintime
-#        maxtime = max(maxtime, tweet_time)  
-#        if(tweet['tweet']['user']['id_str'] not in usrid):
-#            num_followers += tweet['author']['followers']
-#            usrid.add(tweet['tweet']['user']['id_str'])
-#        num_retweet += tweet['metrics']['citations']['total']
-#        line = f.readline()
-#    
-#    f.close()
-#    hourlength = math.ceil((maxtime-mintime)/3600)
-#    print 'The average number of tweets per hour for #%s is %f\n' %(hashtag[i], num_tweet/float(hourlength))
-#    print 'The average number of followers of users for #%s is %f\n' %(hashtag[i], num_followers/float(len(usrid)))
-#    print 'The average number of retweet for #%s is %f\n' %(hashtag[i], (num_retweet+0.0)/num_tweet)
+for i in range(0,len(hashtag)):
+    filename = '../tweet_data/tweets_#%s.txt' % hashtag[i]
+    f = open(filename)
+    line = f.readline()
+    
+    start_date = datetime.datetime(2015,02,01, 12,0,0)
+    end_date = datetime.datetime(2015,02,01, 12,0,0)
+    mintime = int(time.mktime(start_date.timetuple()))
+    maxtime = int(time.mktime(end_date.timetuple()))
+    usrid = set()
+    num_followers = 0
+    num_retweet = 0
+    num_tweet = 0;
+    
+    while(len(line) != 0):
+        tweet = json.loads(line)
+        num_tweet += 1
+        tweet_time = tweet['firstpost_date']
+        new_mintime = min(mintime, tweet_time)
+        if(mintime-new_mintime < 3600*24*15):
+            mintime = new_mintime
+        maxtime = max(maxtime, tweet_time)  
+        if(tweet['tweet']['user']['id_str'] not in usrid):
+            num_followers += tweet['author']['followers']
+            usrid.add(tweet['tweet']['user']['id_str'])
+        num_retweet += tweet['metrics']['citations']['total']
+        line = f.readline()
+    
+    f.close()
+    hourlength = math.ceil((maxtime-mintime)/3600)
+    print 'The average number of tweets per hour for #%s is %f\n' %(hashtag[i], num_tweet/float(hourlength))
+    print 'The average number of followers of users for #%s is %f\n' %(hashtag[i], num_followers/float(len(usrid)))
+    print 'The average number of retweet for #%s is %f\n' %(hashtag[i], (num_retweet+0.0)/num_tweet)
     
 hashtag_plot = ['nfl', 'superbowl']
 #hashtag_plot = ['gohawks']
--- Project4/script/problem6.py
+++ Project4/script/problem6.py
@@ -18,105 +18,105 @@ import string
 import matplotlib.pyplot as plt
 
 
-#hashtag = ['gohawks', 'patriots']
-#start_date = datetime.datetime(2015,02,01, 03,30,0)
-#end_date = datetime.datetime(2015,02,02, 03,30,0)
-#mintime = int(time.mktime(start_date.timetuple()))
-#maxtime = int(time.mktime(end_date.timetuple()))
-#
-#tweets_hawks = [[] for i in range(24)]
-#tweets_patriots = [[] for i in range(24)]
-#
-#for i in range(len(hashtag)):
-#    filename = '../tweet_data/tweets_#%s.txt' % hashtag[i]
-#    f = open(filename)
-#    line = f.readline()
-#    while(len(line) != 0):
-#        tweet = json.loads(line)
-#        if(tweet['firstpost_date'] < mintime or tweet['firstpost_date'] > maxtime):
-#            line = f.readline()
-#            continue
-#        index = (tweet['firstpost_date']-mintime)/3600
-#        if(hashtag[i] == 'gohawks'):
-#            tweets_hawks[index].append(tweet)
-#        else:
-#            tweets_patriots[index].append(tweet)
-#        line = f.readline()        
-#        
-#emoticons_str = r"""
-#    (?:
-#        [:=;] # Eyes
-#        [oO\-]? # Nose (optional)
-#        [D\)\]\(\]/\\OpP] # Mouth
-#    )"""
-# 
-#regex_str = [
-#    emoticons_str,
-#    r'<[^>]+>', # HTML tags
-#    r'(?:@[\w_]+)', # @-mentions
-#    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
-#    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
-# 
-#    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
-#    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
-#    r'(?:[\w_]+)', # other words
-#    r'(?:\S)' # anything else
-#]
-#    
-#tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
-#emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
-#
-#def tokenize(s):
-#    return tokens_re.findall(s)
-# 
-#def preprocess(s, lowercase=True):
-#    tokens = tokenize(s)
-#    if lowercase:
-#        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
-#    return tokens
-#
-#punctuation = list(string.punctuation)
-#stop = stopwords.words('english') + punctuation + ['rt', 'via']
-#
-### building term list and term co-occurrence list for hawks
-#count_list_hawks = []
-#co_occur_list_hawks = []
-#for tweets in tweets_hawks:
-#    count_single_stop = Counter()
-#    co_occur = defaultdict(lambda:defaultdict(int))
-#    for tweet in tweets:
-#        terms_stop = [term for term in preprocess(tweet['tweet']['text']) if term not in stop]
-#        count_single_stop.update(terms_stop)
-#        
-#        for i in range(len(terms_stop)-1):
-#            for j in range(i+1, len(terms_stop)):
-#                w1, w2 = sorted([terms_stop[i], terms_stop[j]])
-#                if(w1 != w2):
-#                    co_occur[w1][w2] += 1
-#                    
-#    #print(count_single_stop.most_common(10))
-#    count_list_hawks.append(count_single_stop)
-#    co_occur_list_hawks.append(co_occur)
-#    
-### building term list and term co-occurrence list for patriots    
-#count_list_patriots = []
-#co_occur_list_patriots = []
-#for tweets in tweets_patriots:
-#    count_single_stop = Counter()
-#    co_occur = defaultdict(lambda:defaultdict(int))
-#    for tweet in tweets:
-#        terms_stop = [term for term in preprocess(tweet['tweet']['text']) if term not in stop]
-#        count_single_stop.update(terms_stop)
-#        
-#        for i in range(len(terms_stop)-1):
-#            for j in range(i+1, len(terms_stop)):
-#                w1, w2 = sorted([terms_stop[i], terms_stop[j]])
-#                if(w1 != w2):
-#                    co_occur[w1][w2] += 1
-#                    
-#    #print(count_single_stop.most_common(10))
-#    count_list_patriots.append(count_single_stop)
-#    co_occur_list_patriots.append(co_occur)
+hashtag = ['gohawks', 'patriots']
+start_date = datetime.datetime(2015,02,01, 03,30,0)
+end_date = datetime.datetime(2015,02,02, 03,30,0)
+mintime = int(time.mktime(start_date.timetuple()))
+maxtime = int(time.mktime(end_date.timetuple()))
+
+tweets_hawks = [[] for i in range(24)]
+tweets_patriots = [[] for i in range(24)]
+
+for i in range(len(hashtag)):
+    filename = '../tweet_data/tweets_#%s.txt' % hashtag[i]
+    f = open(filename)
+    line = f.readline()
+    while(len(line) != 0):
+        tweet = json.loads(line)
+        if(tweet['firstpost_date'] < mintime or tweet['firstpost_date'] > maxtime):
+            line = f.readline()
+            continue
+        index = (tweet['firstpost_date']-mintime)/3600
+        if(hashtag[i] == 'gohawks'):
+            tweets_hawks[index].append(tweet)
+        else:
+            tweets_patriots[index].append(tweet)
+        line = f.readline()        
+        
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+    
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+def preprocess(s, lowercase=True):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+punctuation = list(string.punctuation)
+stop = stopwords.words('english') + punctuation + ['rt', 'via']
+
+## building term list and term co-occurrence list for hawks
+count_list_hawks = []
+co_occur_list_hawks = []
+for tweets in tweets_hawks:
+    count_single_stop = Counter()
+    co_occur = defaultdict(lambda:defaultdict(int))
+    for tweet in tweets:
+        terms_stop = [term for term in preprocess(tweet['tweet']['text']) if term not in stop]
+        count_single_stop.update(terms_stop)
+        
+        for i in range(len(terms_stop)-1):
+            for j in range(i+1, len(terms_stop)):
+                w1, w2 = sorted([terms_stop[i], terms_stop[j]])
+                if(w1 != w2):
+                    co_occur[w1][w2] += 1
+                    
+    #print(count_single_stop.most_common(10))
+    count_list_hawks.append(count_single_stop)
+    co_occur_list_hawks.append(co_occur)
+    
+## building term list and term co-occurrence list for patriots    
+count_list_patriots = []
+co_occur_list_patriots = []
+for tweets in tweets_patriots:
+    count_single_stop = Counter()
+    co_occur = defaultdict(lambda:defaultdict(int))
+    for tweet in tweets:
+        terms_stop = [term for term in preprocess(tweet['tweet']['text']) if term not in stop]
+        count_single_stop.update(terms_stop)
+        
+        for i in range(len(terms_stop)-1):
+            for j in range(i+1, len(terms_stop)):
+                w1, w2 = sorted([terms_stop[i], terms_stop[j]])
+                if(w1 != w2):
+                    co_occur[w1][w2] += 1
+                    
+    #print(count_single_stop.most_common(10))
+    count_list_patriots.append(count_single_stop)
+    co_occur_list_patriots.append(co_occur)
 
     
 # calculate the PMI for terms
